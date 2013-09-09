86ffd764335687ac18ba7f486dd2f677b2cab181
Hareesan <rharishan@gmail.com>
2013-09-09 23:29:40 +0530

    Added RSA libraries

--- chrome/content/js/browserOverlay.js
+++ chrome/content/js/browserOverlay.js
@@ -26,7 +26,58 @@ Overlay.CustomButton = {
     click: function (aEvent) {
         Overlay.PopUpNode.init(aEvent);
         var stegoDataHandler = new StegoDataHandler();
-        /*window.alert("test");
+
+        var jsLoader = Components.classes["@mozilla.org/moz/jssubscript-loader;1"]
+            .getService(Components.interfaces.mozIJSSubScriptLoader);
+        jsLoader.loadSubScript("chrome://browser-stego/content/js/rsa/BigInteger.init1.js");
+
+        jsLoader.loadSubScript("chrome://browser-stego/content/js/rsa/RSA.init1.js");
+
+        jsLoader.loadSubScript("chrome://browser-stego/content/js/rsa/SecureRandom.js");
+
+        jsLoader.loadSubScript("chrome://browser-stego/content/js/rsa/BigInteger.init2.js");
+        jsLoader.loadSubScript("chrome://browser-stego/content/js/rsa/RSAKeyFormat.js");
+        jsLoader.loadSubScript("chrome://browser-stego/content/js/rsa/RSA.init2.js");
+        jsLoader.loadSubScript("chrome://browser-stego/content/js/rsa/packages.js");
+        jsLoader.loadSubScript("chrome://browser-stego/content/js/rsa/elapse.js");
+        jsLoader.loadSubScript("chrome://browser-stego/content/js/rsa/isarray.js");
+
+        __unit( "stego.xul" );
+        __uses( "packages.js" );
+        __uses( "RSAKeyFormat.js" );
+        __uses( "RSAMessageFormat.js" );
+        __uses( "RSAMessageFormatSOAEP.js" );
+        __uses( "RSAMessageFormatBitPadding.js" );
+        __uses( "RSA.init1.js" );
+        __uses( "RSA.init2.js" );
+
+
+        // Import classes.
+        var RSA = __import(this, "titaniumcore.crypto.RSA");
+        var BigInteger = __import(this, "titaniumcore.crypto.BigInteger");
+
+        var RSA = __import( this, "titaniumcore.crypto.RSA" );
+        var RSAMessageFormatSOAEP = __import( this, "titaniumcore.crypto.RSAMessageFormatSOAEP" );
+        var RSAMessageFormatBitPadding = __import( this, "titaniumcore.crypto.RSAMessageFormatBitPadding" );
+        var RSAKeyFormat = __import( packageRoot, "titaniumcore.crypto.RSAKeyFormat" );
+
+        // install key format.
+        RSA.installKeyFormat( RSAKeyFormat )
+        RSA.installMessageFormat( RSAMessageFormatSOAEP );
+
+        // Create an RSA engine.
+        var rsa = new RSA();
+        // Generate new RSA key.
+        var test = rsa.generate(128, 65537);
+        window.alert("test");
+
+
+
+        /*window.open(
+            "chrome://browser-stego/content/test.xul",
+            "stego-contextencrypt",
+            "chrome,centerscreen");
+        window.alert("test");
          stegoDataHandler.addKeyPairs("testiw","testlk");
          window.alert(stegoDataHandler.getKeyPairs("testiw"));*/
     }
